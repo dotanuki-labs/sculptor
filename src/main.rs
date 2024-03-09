ff2a4b1c1b90daa1a241bd8e6680b14a38d9e17f
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-03-09 22:36:53 +0100

    Implements simple app and adds tests (#11)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct ProgramArguments {
+    #[arg(short, long)]
+    name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = ProgramArguments::parse();
+    println!("Hello, {}!", arguments.name);
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_cmd::Command;
+    use predicates::str::contains;
+
+    static TOOL: &str = "rust-cli-tool-scaffold";
+
+    #[test]
+    fn should_parse_arguments() {
+        let mut cmd = Command::cargo_bin(TOOL).unwrap();
+        let assert = cmd.args(["--name", "John"]).assert();
+
+        let expected = "Hello, John!\n";
+        assert.stdout(expected);
+    }
+
+    #[test]
+    fn should_show_help() {
+        let mut cmd = Command::cargo_bin(TOOL).unwrap();
+        let description = "An opinionated way to kick-off CLI apps powered by Rust";
+
+        let assert = cmd.arg("--help").assert();
+        assert.stdout(contains(description));
+    }
+
+    #[test]
+    fn should_fail_without_arguments() {
+        let mut cmd = Command::cargo_bin(TOOL).unwrap();
+        let instruction = "required arguments were not provided";
+
+        let assert = cmd.assert();
+        assert.failure().stderr(contains(instruction));
+    }
 }

2fb23b1714c633fd6bf9f495d143553fdeabd200
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-09-15 14:11:53 +0200

    feat(ci): migrate to warpbuild (#427)

--- crates/xtasks/src/sources.rs
+++ crates/xtasks/src/sources.rs
@@ -1,9 +1,6 @@
 // Copyright 2025 Dotanuki Labs
 // SPDX-License-Identifier: MIT
 
-use crate::utils::{
-    BuildEnvironment::CI, BuildEnvironment::Local, docker_execution_arguments, evaluate_build_environment,
-};
 use xshell::{Shell, cmd};
 
 pub fn check_source_files(shell: &Shell) -> anyhow::Result<()> {
@@ -17,15 +14,7 @@ fn check_code_formatting(shell: &Shell) -> anyhow::Result<()> {
     println!("🔥 Checking formatting of Rust code (rustfmt)");
     println!();
 
-    match evaluate_build_environment() {
-        CI => {
-            let (volume, image) = docker_execution_arguments();
-            cmd!(shell, "docker run --rm -v {volume} {image} fmt").run()?;
-        },
-        Local => {
-            cmd!(shell, "cargo fmt --check").run()?;
-        },
-    };
+    cmd!(shell, "cargo fmt --check").run()?;
 
     Ok(())
 }
@@ -35,15 +24,7 @@ fn check_code_smells(shell: &Shell) -> anyhow::Result<()> {
     println!("🔥 Checking smells in Rust code (clippy)");
     println!();
 
-    match evaluate_build_environment() {
-        CI => {
-            let (_, _) = docker_execution_arguments();
-            cmd!(shell, "cargo clippy --all-targets --all-features -- -D warnings").run()?;
-        },
-        Local => {
-            cmd!(shell, "cargo clippy --all-targets --all-features -- -D warnings").run()?;
-        },
-    };
+    cmd!(shell, "cargo clippy --all-targets --all-features -- -D warnings").run()?;
 
     Ok(())
 }

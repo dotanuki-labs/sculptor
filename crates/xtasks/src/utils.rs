81a0d74979e98afa75d8590ad41e47bdf77efb07
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2025-12-24 05:40:00 +0100

    fix(ci): use latest callinectes docker image

--- crates/xtasks/src/utils.rs
+++ crates/xtasks/src/utils.rs
@@ -5,7 +5,6 @@ use crate::utils::BuildEnvironment::{CI, Local};
 use std::env;
 
 static CALLINECTES_DOCKER_IMAGE: &str = "ghcr.io/dotanuki-labs/callinectes:latest";
-static CALLINECTES_DOCKER_DIGEST: &str = "3a5252044164b7aca1ebb7d23c850177a7c280fb91d277d2b88dcdb4b9d03571";
 
 pub enum BuildEnvironment {
     CI,
@@ -20,10 +19,9 @@ pub fn evaluate_build_environment() -> BuildEnvironment {
 }
 
 pub fn docker_execution_arguments() -> (String, String) {
-    let docker_package = format!("{CALLINECTES_DOCKER_IMAGE}@sha256:{CALLINECTES_DOCKER_DIGEST}");
     let current_dir = env::current_dir().expect("Failed to get current directory");
     let pwd = current_dir
         .to_str()
         .expect("Failed to convert current directory to string");
-    (format!("{pwd}:/usr/src"), docker_package)
+    (format!("{pwd}:/usr/src"), CALLINECTES_DOCKER_IMAGE.to_string())
 }
